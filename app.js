b43faf99d209c275c5cede5a72324261b5261786
flyingfoxweb <erin@flyingfox.net.nz>
2017-06-08 22:59:44 +0000

    Mongoose required. Schema and model created for campgrounds and routes updated to use Campground.create and Campground.find.

--- app.js
+++ app.js
@@ -1,41 +1,59 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelpcamp");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
 
-//Create a temp campgrounds array
-var campgrounds = [
-        {
-            name: "Campground 1",
-            image: "http://www.whcg.net/images/summer.jpg"
-        },
-        {
-            name: "Campground 2",
-            image: "http://www.truckcampermagazine.com/wp-content/uploads/stories/Yellowstone-National-Park/Yellowstone-buffalo-11.jpg"
-        },
-        {
-            name: "Campground 3",
-            image: "https://media-cdn.tripadvisor.com/media/photo-s/02/b4/bb/94/french-broad-river-campground.jpg"
-        }
-    ];
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Campground Two",
+//     image: "http://www.truckcampermagazine.com/wp-content/uploads/stories/Yellowstone-National-Park/Yellowstone-buffalo-11.jpg"
+// }, function(err, newlyCreated) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log("Campground added..");
+//         console.log(newlyCreated);
+//     }
+// });
+
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", {campgrounds:campgrounds});
+    Campground.find({}, function(err, allCampgrounds) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(newlyCreated);
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 app.get("/campgrounds/new", function(req, res) {
