c2f614cf44ab4ab5eec81b51f5258fea40c34cab
flyingfoxweb <erin@flyingfox.net.nz>
2017-06-08 03:12:48 +0000

    Partials folder created and header.ejs and footer.ejs files created. Campgrounds get route created with campgrounds.ejs template and a forEach loop to display each campground to the page.

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 var express = require("express");
 var app = express();
+// var bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+// app.use(bodyParser.urlencoded({extended: true}));
 
 
+//Create a temp campgrounds array
+var campgrounds = [
+        {
+            name: "Campground 1",
+            img: "http://www.whcg.net/images/summer.jpg"
+        },
+        {
+            name: "Campground 2",
+            img: "http://www.truckcampermagazine.com/wp-content/uploads/stories/Yellowstone-National-Park/Yellowstone-buffalo-11.jpg"
+        },
+        {
+            name: "Campground 3",
+            img: "https://media-cdn.tripadvisor.com/media/photo-s/02/b4/bb/94/french-broad-river-campground.jpg"
+        }
+    ];
 
+app.get("/", function(req, res) {
+    res.render("landing");
+});
 
-
+app.get("/campgrounds", function(req, res) {
+    res.render("campgrounds", {campgrounds:campgrounds});
+});
 
 
 
