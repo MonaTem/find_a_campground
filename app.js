05b6b61ffb386e3188d7fa044b108143e2a7237a
flyingfoxweb <erin@flyingfox.net.nz>
2017-06-09 00:08:23 +0000

    Show route and template created to show the description of the campground which is hidden on the index.ejs page.

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
-//     name: "Campground Two",
-//     image: "http://www.truckcampermagazine.com/wp-content/uploads/stories/Yellowstone-National-Park/Yellowstone-buffalo-11.jpg"
+//     name: "Campground One",
+//     image: "http://www.truckcampermagazine.com/wp-content/uploads/stories/Yellowstone-National-Park/Yellowstone-buffalo-11.jpg",
+//     description: "This is the first campground."
 // }, function(err, newlyCreated) {
 //     if(err) {
 //         console.log(err);
@@ -32,20 +34,25 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+
+//INDEX route - show all campgrounds
 app.get("/campgrounds", function(req, res) {
     Campground.find({}, function(err, allCampgrounds) {
         if(err) {
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds:allCampgrounds});
+            res.render("index", {campgrounds:allCampgrounds});
         }
     });
 });
 
+//CREATE route - add a new campground to the db
+
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description};
     Campground.create(newCampground, function(err, newlyCreated) {
         if(err) {
             console.log(err);
@@ -56,11 +63,25 @@ app.post("/campgrounds", function(req, res) {
     });
 });
 
+//NEW route - shows the form to add a new campground
+
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
 });
 
+//SHOW route - show the information about a particular item.
 
+app.get("/campgrounds/:id", function(req, res) {
+    //Find the campground with the provided ID
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if(err) {
+            console.log(err);
+        } else {
+            //Render the show template with the info from that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
 
 
 
