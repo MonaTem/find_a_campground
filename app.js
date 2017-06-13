128a033a15edacd4ea21c4e5b4d6b16e3657f1b1
flyingfoxweb <erin@flyingfox.net.nz>
2017-06-13 23:36:52 +0000

    Campground route files moved into the views/campgrounds folder and comment route files moved into views/comments folder.  JS file and route created to show the form to create a new comment and post route created for a new comment to actually be added to a specific campground.

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get("/campgrounds", function(req, res) {
         if(err) {
             console.log(err);
         } else {
-            res.render("index", {campgrounds:allCampgrounds});
+            res.render("campgrounds/index", {campgrounds:allCampgrounds});
         }
     });
 });
@@ -53,7 +53,7 @@ app.post("/campgrounds", function(req, res) {
 //NEW route - shows the form to add a new campground
 
 app.get("/campgrounds/new", function(req, res) {
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 //SHOW route - show the information about a particular item.
@@ -66,13 +66,46 @@ app.get("/campgrounds/:id", function(req, res) {
         } else {
             console.log(foundCampground);
             //Render the show template with the info from that campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
 });
 
+//COMMENTS ROUTES
+//=====================================================================
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
 
-
+app.post("/campgrounds/:id/comments", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground) {
+        if(err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save(function(err) {
+                        if(err) {
+                            console.log(err);
+                        } else {
+                            res.redirect("/campgrounds/" + campground._id);
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function() {
