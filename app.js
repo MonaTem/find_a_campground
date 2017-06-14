981a67d81a4ff354617fa7bde0a15cb4a94e546f
flyingfoxweb <erin@flyingfox.net.nz>
2017-06-14 23:20:14 +0000

    Added the get and post routes for the login functionality.

--- app.js
+++ app.js
@@ -145,6 +145,21 @@ app.post("/register", function(req, res) {
     });
 });
 
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res) {
+});
+
+
+
+//Tell the server to listen for requests
+//=====================================================================
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("The server has started..");
