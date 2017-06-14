3d0e4fb247e7291e43bcf942edb74e20090edb43
flyingfoxweb <erin@flyingfox.net.nz>
2017-06-14 00:41:48 +0000

    Added a style.css file in the public directory. Added some additional custom and bootstrap styling to the show.ejs page.

--- seeds.js
+++ seeds.js
@@ -4,19 +4,19 @@ var Comment = require("./models/comments");
 
 var data = [
         {
-            name: "First campground",
+            name: "Grassy Plains Campground",
             image: "https://www.nps.gov/shen/planyourvisit/images/Campgrounds_1.jpg",
-            description: "A campground with a bbq and benches."
+            description: "Marvels mighty mutants go worldwide and beyond in this series following Cyclops, Wolverine, Beast, Emma Frost and more in their astonishing adventures. Amazing Spider-Man is the cornerstone of the Marvel Universe. Born with super-human senses and the power to heal from almost any wound, Wolverine was captured by a secret Canadian organization and given an unbreakable skeleton and claws. Now, hes a premiere member of both the X-Men and the Avengers."
         },
         {
-            name: "Second campground",
+            name: "Snowy Peak",
             image: "http://www.visitcentraliowa.com/images/campground.jpg",
-            description: "Lots and lots of campervans."
+            description: "Marvels mighty mutants go worldwide and beyond in this series following Cyclops, Wolverine, Beast, Emma Frost and more in their astonishing adventures. Amazing Spider-Man is the cornerstone of the Marvel Universe. Born with super-human senses and the power to heal from almost any wound, Wolverine was captured by a secret Canadian organization and given an unbreakable skeleton and claws. Now, hes a premiere member of both the X-Men and the Avengers."
         },
         {
-            name: "Third campground",
+            name: "Desert Mesa",
             image: "http://camprrm.com/wp-content/uploads/2012/02/widewaters-campground-1.jpg",
-            description: "Just a lake...."
+            description: "Marvels mighty mutants go worldwide and beyond in this series following Cyclops, Wolverine, Beast, Emma Frost and more in their astonishing adventures. Amazing Spider-Man is the cornerstone of the Marvel Universe. Born with super-human senses and the power to heal from almost any wound, Wolverine was captured by a secret Canadian organization and given an unbreakable skeleton and claws. Now, hes a premiere member of both the X-Men and the Avengers."
         }
     ];
 
