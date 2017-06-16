5ac2ec6762a1dd9d1d816c5863c9f2d129b51b2e
flyingfoxweb <erin@flyingfox.net.nz>
2017-06-16 04:51:58 +0000

    Add authorisation capabilities so only the user that's created the campground will be able to edit or delete the campground.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -61,20 +61,15 @@ router.get("/:id", function(req, res) {
 
 //EDIT route
 
-router.get("/:id/edit", function(req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
     Campground.findById(req.params.id, function(err, foundCampground) {
-        if(err) {
-            console.log(err);
-            res.redirect("/campgrounds");
-        } else {
-            res.render("campgrounds/edit", {campground: foundCampground});
-        }
+        res.render("campgrounds/edit", {campground: foundCampground});
     });
 });
 
 //UPDATE route
 
-router.put("/:id", function(req, res) {
+router.put("/:id", checkCampgroundOwnership, function(req, res) {
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
         if(err) {
             console.log(err);
@@ -87,7 +82,7 @@ router.put("/:id", function(req, res) {
 });
 
 //DELETE route
-router.delete("/:id", function(req, res) {
+router.delete("/:id", checkCampgroundOwnership, function(req, res) {
     Campground.findByIdAndRemove(req.params.id, function(err) {
         if(err) {
             console.log(err);
@@ -99,13 +94,34 @@ router.delete("/:id", function(req, res) {
     });
 });
 
-
+//MIDDLEWARE
 
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()) {
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next) {
+    //Is user logged in at all
+    if(req.isAuthenticated()) {
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if(err) {
+                res.redirect("back");
+            } else {
+                //Does the user own the campground?
+                if(foundCampground.author.id.equals(req.user._id)) {    //.equals is a mongoose method
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        //If not, redirect
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
