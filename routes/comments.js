ce35e6caf14abc38fafc446f3cb665bcaf0ca115
flyingfoxweb <erin@flyingfox.net.nz>
2017-06-22 23:26:12 +0000

    Added authentication to the comments. The edit comment route can now only be accessed if the logged in user is the user that created the comment.

--- routes/comments.js
+++ routes/comments.js
@@ -46,7 +46,7 @@ router.post("/", isLoggedIn, function(req, res) {
     });
 });
 
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment) {
         if(err) {
             res.redirect("back");
@@ -57,7 +57,7 @@ router.get("/:comment_id/edit", function(req, res) {
     
 });
 
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
         if(err) {
             res.redirect("back");
@@ -85,6 +85,27 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+    //Is user logged in at all
+    if(req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if(err) {
+                res.redirect("back");
+            } else {
+                //Does the user own the comment?
+                if(foundComment.author.id.equals(req.user._id)) {    //.equals is a mongoose method - have to use this b/c req.user._id is a string but foundCampground.author.id is a js object.
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        //If not, redirect
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
